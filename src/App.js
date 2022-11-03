a7ad5a1752deb2ad8e2967f2319551171482cd3e
Ben Baldwin <baldwin.ben55@gmail.com>
2022-11-02 19:28:21 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+const [pokemon, setPokemon] = useState([])
+
+// on button submit, run fetchPokemon which grabs the object and sets it in state.
+const fetchPokemon = () => {
+  axios.get("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0.json")
+    .then(response => {
+      console.log(response.data.results);
+      setPokemon(response.data.results);
+    })
+    .catch(err => console.log(err))
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button onClick={fetchPokemon}>Catch em all!</button>
+      <hr />
+      {
+        pokemon.map((pokemon, i) => {
+          return (
+            <li>{pokemon.name}</li>
+          )
+        })
+      }
     </div>
   );
 }
